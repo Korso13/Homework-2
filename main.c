674a02a6d723baead1dd39637b73dd766c084066
Tim Korso <ktv91@bk.ru>
2022-03-16 22:54:54 +0300

    First draft of message printing prog

--- main.c
+++ main.c
@@ -2,6 +2,15 @@
 
 int main()
 {
-    printf("Hello New World!\n");
+    printf("Goodbye Old World!\n");
+    printf("Hello \'New One\'!\n");
+    printf("Reverse \\ slash is so slashy\n");
+    printf("what about \t some Tabulation\?\n");
+    //printf("Terminant is a weird \f \n \0 TERM");
+    printf("What is backspace\b\b \n");
+    int a = 50;
+    printf("%d\n", a);
+    printf("%05i\n", a);
+    printf("%.2f \n", 5.2135123);
     return 0;
 }
